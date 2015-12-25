6be00d047e9b0dee59f4cf81d5c9aab3561d173a
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-12-25 13:16:21 -0200

    Added base module, based on a fork from a client, from Joomla 3.4.6 mod_banners

---
+++ helper.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_banners4varnish
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Alligo Ltda. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_banners4varnish
+ *
+ * @package     Joomla.Site
+ * @subpackage  mod_banners4varnish
+ * @since       1.5
+ */
+class ModBannersHelper
+{
+
+    /**
+     * Retrieve list of banners
+     *
+     * @param   \Joomla\Registry\Registry  &$params  module parameters
+     *
+     * @return  mixed
+     */
+    public static function &getList(&$params)
+    {
+        JModelLegacy::addIncludePath(JPATH_ROOT . '/components/com_banners/models', 'BannersModel');
+        $document = JFactory::getDocument();
+        $app = JFactory::getApplication();
+        $keywords = explode(',', $document->getMetaData('keywords'));
+
+        $model = JModelLegacy::getInstance('Banners', 'BannersModel', array('ignore_request' => true));
+        $model->setState('filter.client_id', (int) $params->get('cid'));
+        $model->setState('filter.category_id', $params->get('catid', array()));
+        $model->setState('list.limit', (int) $params->get('count', 1));
+        $model->setState('list.start', 0);
+        $model->setState('filter.ordering', $params->get('ordering'));
+        $model->setState('filter.tag_search', $params->get('tag_search'));
+        $model->setState('filter.keywords', $keywords);
+        $model->setState('filter.language', $app->getLanguageFilter());
+
+        $banners = $model->getItems();
+        $model->impress();
+
+        return $banners;
+    }
+
+    /**
+     * Retorna HTML do modulo via requisição AJAX. Exemplo de URL:
+     *
+     * /?option=com_ajax&module=banners&format=raw&bannerid=189&no_cache=1450513754247
+     *
+     * @author  Emerson Rocha Luiz <emerson@alligo.com.br>
+     *
+     * return String
+     */
+    public static function getAjax()
+    {
+        //$modules = JModuleHelper::getModuleList();
+        $banner_id = JFactory::getApplication()->input->getInt('bannerid', null);
+//        $modules = JModuleHelper::getModuleList();
+//        if (!empty($modules) && is_array($modules)) {
+//            foreach ($modules AS $module) {
+//                if ((int) $module->id === $banner_id) {
+//                    $modulo_agora = $module;
+//                    break;
+//                }
+//            }
+//        }
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true)
+            ->select('m.title')
+            ->from('#__modules AS m')
+            ->where('m.published = 1 and m.id = ' . (int) $banner_id)
+            ;
+        $db->setQuery($query);
+        $modulo_agora_title = $db->loadResult();
+
+        //var_dump($banner_id, $modulo_agora, $modules);
+
+        if ($modulo_agora_title) {
+            $module = JModuleHelper::getModule('banners', $modulo_agora_title);
+            $registry = new JRegistry;
+            $params = $registry->loadString($module->params);
+            $list = self::getList($params);
+            require JModuleHelper::getLayoutPath('mod_banners4varnish', 'default');
+        } else {
+            return '<!-- mod_banners4varnish getAjax: erro ao obter modulo -->';
+        }
+    }
+}
---
+++ tmpl/ajax.php
@@ -0,0 +1,48 @@
+<?php
+defined('_JEXEC') or die;
+//print_r($params);
+//print_r($list);
+$modules = JModuleHelper::getModuleList();
+//echo '<pre>' ; var_dump($params); echo '</pre>';
+
+//echo '<pre>' ; var_dump($params, htmlentities(json_encode($modules))); echo '</pre>';
+$id_modulo = null;
+foreach ($modules AS $module) {
+    if ($module->module === 'mod_banners4varnish') {
+        if ($module->params) {
+            $registry = new JRegistry;
+            $module_params = $registry->loadString($module->params);
+
+            // Esse modulo não recebe ID de uma forma amigável. Infelismente, é necessário
+            // descobrir por força quase bruta
+            if ($module_params['catid'] === $params['catid'] &&
+                $module_params['ordering'] === $params['ordering'] &&
+                $module_params['header_text'] === $params['header_text'] &&
+                $module_params['footer_text'] === $params['footer_text'] &&
+                $module_params['moduleclass_sfx'] === $params['moduleclass_sfx']
+            ) {
+                //var_dump($module_params->catid, $params->catid, $params['catid'], $module_params->ordering, $module_params->header_text, $params);die;
+                $id_modulo = $module->id;
+                break;
+            }
+            //var_dump($module);
+            //var_dump($module_params->layout);
+            //die;
+        }
+    }
+}
+//var_dump($module);
+//die;
+if (!empty($id_modulo)) :
+?>
+<div class="banner-ajax" id="banner-ajax<?= $id_modulo ?>"></div>
+<script>
+    // Carrega o banner entre 50 a 200ms
+    setTimeout(function() {
+        jQuery("#banner-ajax<?= $id_modulo ?>").load("/index.php?option=com_ajax&module=banners&format=raw&bannerid=<?= $id_modulo ?>&no_cache=" + (new Date().getTime()));
+    }, (Math.floor(Math.random() * (200 - 50)) + 50));
+    //}, 500 + (Math.floor(Math.random() * (600 - 100)) + 100));
+</script>
+<?php else: ?>
+<!-- Houve algum erro, e este banner não pôde ser carregado -->
+<?php endif; ?>
